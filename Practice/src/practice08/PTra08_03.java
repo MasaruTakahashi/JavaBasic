c5bd7fde6b51b0c1a0ad5608ac720087ceb0ce73
MasaruTakahashi <MasaruTakahashi@users.noreply.github.com>
2019-03-04 18:46:05 +0900

    9-2

--- Practice/src/practice08/PTra08_03.java
+++ Practice/src/practice08/PTra08_03.java
@@ -11,11 +11,16 @@ public class PTra08_03 {
 
 		// ★ 0～100のランダムな値を取得して、変数numberに代入してください
 
+		int number =  new java.util.Random().nextInt(100);
 
 		// ★ 変数resultを宣言し、メソッドisEvenNumberを（変数number）で呼び出した結果を代入してください
 
+		boolean result = isEvenNumber(number);
 
-		if (result) {
+
+
+		if (result==true) {
+
 			System.out.println("偶数です");
 		} else {
 			System.out.println("奇数です");
@@ -31,4 +36,20 @@ public class PTra08_03 {
 	 *
 	 * 引数で渡された内容が偶数ならtrue、奇数ならfalseを返すメソッド
 	 */
+
+	public static boolean isEvenNumber(int x) {
+
+		boolean a = true;
+
+		if(x%2==0) {
+		a=true;
+
+		}else {
+		a =false;
+
+		}
+		return a ;
+
+
+	}
 }
--- Practice/src/practice08/PTra08_05.java
+++ Practice/src/practice08/PTra08_05.java
@@ -33,7 +33,15 @@ public class PTra08_05 {
 
 		// ★ 変数choiceの中が、1であれば四角形の面積を算出するメソッドを、2であれば三角形の面積を算出するメソッドを呼び出してください
 
+		if(choice==1) {
+		System.out.println(quadrangle(width, height));
+		}else {
+			System.out.println(triangle(width, height));
+		}
+
 	}
+
+
 
 	/*
 	 * ★ 以下の仕様でメソッドを作成してください
@@ -44,6 +52,13 @@ public class PTra08_05 {
 	 *
 	 * 引数で渡された横の長さ(width)と縦の長さ(height)から、四角形の面積を返すメソッド
 	 */
+	public static double quadrangle(double width, double height) {
+		double a = width * height;
+		return a;
+
+	}
+
+
 
 
 	/*
@@ -55,5 +70,10 @@ public class PTra08_05 {
 	 *
 	 * 引数で渡された横の長さ(width)と縦の長さ(height)から、三角形の面積を返すメソッド
 	 */
+	public static double triangle(double width, double height) {
+		double b =width * height / 2;
+		return b;
+
+	}
 
 }
